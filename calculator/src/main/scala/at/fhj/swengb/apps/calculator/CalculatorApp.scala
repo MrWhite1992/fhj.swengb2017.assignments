f85f63d22e4a5d8779bd5dca9171d4ee1fcbb13f
MrWhite1992 <ruggenthalerbenjamin@gmx.at>
2017-12-11 16:56:12 +0100

    Calculator with finished gui and working tests CalculatorApp is still buggy and needs solutions. No time left to finish

--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/CalculatorApp.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/CalculatorApp.scala
@@ -3,10 +3,13 @@ package at.fhj.swengb.apps.calculator
 import java.net.URL
 import java.util.ResourceBundle
 import javafx.application.Application
-import javafx.fxml.{FXMLLoader, Initializable}
+import javafx.beans.property.{ObjectProperty, SimpleObjectProperty}
+import javafx.fxml.{FXML, FXMLLoader, Initializable}
+import javafx.scene.control.TextField
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
+import scala.util.{Failure, Success}
 import scala.util.control.NonFatal
 
 object CalculatorApp {
@@ -19,7 +22,7 @@ object CalculatorApp {
 class CalculatorFX extends javafx.application.Application {
 
   val fxml = "/at/fhj/swengb/apps/calculator/calculator.fxml"
-  val css =  "/at/fhj/swengb/apps/calculator/calculator.css"
+  val css = "/at/fhj/swengb/apps/calculator/calculator.css"
 
   def mkFxmlLoader(fxml: String): FXMLLoader = {
     new FXMLLoader(getClass.getResource(fxml))
@@ -46,126 +49,129 @@ class CalculatorFX extends javafx.application.Application {
 }
 
 class CalculatorFxController extends Initializable {
+
+  val calculatorProperty: ObjectProperty[RpnCalculator] = new SimpleObjectProperty[RpnCalculator](RpnCalculator())
+
+  def getCalculator() : RpnCalculator = calculatorProperty.get()
+
+  def setCalculator(rpnCalculator : RpnCalculator) : Unit = calculatorProperty.set(rpnCalculator)
+
+  @FXML var numberTextField1 : TextField = _
+  @FXML var numberTextField2 : TextField = _
+  @FXML var numberTextField3 : TextField = _
+
   override def initialize(location: URL, resources: ResourceBundle) = {
 
   }
 
-  /** Liste für jeden knopfdruck zum zwischenspeichern
-    * Liste für jede operation zum zwischenspeichern
-    * Liste für das ergebnis.
-    * funktion um die nächste eingabe zu bestätigen; sobald eine Op. eingegeben wird muss eine zahl Folgen,
-    * falls eine weitere Op folgt -> liste für zwischenergebnis, falls = dann ergebnis
-    *   ( Zahl -> Operation -> Zahl .... -> Ergebnis)
-    *
-    *einfacher ansatz -> IF - Else JUNGLE....!
-    *
-    */
+  def calc(): Unit = {
+    getCalculator().push(Op(numberTextField1.getText)) match {
+      case Success(c) => setCalculator(c)
+      case Failure(e) => // show warning / error
+    }
+    getCalculator().stack foreach println
+
+    (numberTextField2.setText(getCalculator().stack(0).toString.drop(4).init))
+    numberTextField3.setText("")
+    (numberTextField1.setText(getCalculator().stack(0).toString.drop(4).init))
+    numberTextField3.setText("")
+
+  }
+
+
 
   def one() : Unit = {
-    println("1 clicked")
+    numberTextField3.appendText("1")
 
   }
 
   def two() : Unit = {
-    println("2 clicked")
+    numberTextField3.appendText("2")
 
   }
 
   def three() : Unit = {
-    println("3 clicked")
+    numberTextField3.appendText("3")
 
   }
 
   def four() : Unit = {
-    println("4 clicked")
+    numberTextField3.appendText("4")
 
   }
 
   def five() : Unit = {
-    println("5 clicked")
+    numberTextField3.appendText("5")
 
   }
 
   def six() : Unit = {
-    println("6 clicked")
+    numberTextField3.appendText("6")
 
   }
 
   def seven() : Unit = {
-    println("7 clicked")
+    numberTextField3.appendText("7")
 
   }
 
   def eight() : Unit = {
-    println("8 clicked")
+    numberTextField3.appendText("8")
 
   }
 
   def nine() : Unit = {
-    println("9 clicked")
+    numberTextField3.appendText("9")
 
   }
 
   def zero() : Unit = {
-    println("0 clicked")
+    numberTextField3.appendText("0")
 
   }
 
   def add() : Unit = {
-    println("+ clicked")
+    numberTextField3.appendText("+")
 
   }
 
   def subst() : Unit = {
-    println("- clicked")
+    numberTextField3.appendText("-")
 
   }
 
   def multi() : Unit = {
-    println("X clicked")
+    numberTextField3.appendText("*")
 
   }
 
   def change() : Unit = {
-    println("+/- clicked")
+    numberTextField3.appendText("+/-")
 
   }
 
   def div() : Unit = {
-    println("/ clicked")
+    numberTextField3.appendText("/")
 
   }
 
   def comma() : Unit = {
-    println(", clicked")
+    numberTextField3.appendText(",")
 
   }
 
   def stack() : Unit = {
-    println("INTERIM clicked")
-
-  }
-
-  def calc() : Unit = {
-    println("= clicked")
+    numberTextField3.appendText("Interim")
 
   }
 
   def perc() : Unit = {
-    println("% clicked")
-
+    numberTextField3.appendText("%")
   }
 
 
   def ac() : Unit = {
-    println("AC clicked")
-
+    numberTextField3.appendText("")
   }
-
-  def sgn() : Unit = {
-    println("SCheisse")
-  }
-
 
 }
-
